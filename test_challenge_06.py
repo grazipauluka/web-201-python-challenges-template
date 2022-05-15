4424476b7080e55907449bca0b2661198ca898bd
Jyotsna Gorle <jyotsnagorle@gmail.com>
2022-05-15 12:11:22 +0200

    additions for dictionaries and arrays

---
+++ test_challenge_06.py
@@ -0,0 +1,9 @@
+# Write a function in python that 
+# takes an array of numbers and returns a
+# sum of the square of all the numbers in the array.
+
+def sum_of_squares(array_of_numbers):
+    pass
+
+def test_challenge_06_happy_case(): 
+     assert sum_of_squares([1,2,3,4]) == 30
